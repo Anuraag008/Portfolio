07432d0582d371f2e4efa18dd75325d827ae7cf2
anuraag <anuraagkumar7678@gmail.com>
2026-06-15 13:58:50 +0530

    change about section

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -57,7 +57,7 @@ export const About = () => {
               <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
                 <h3 className="text-xl font-bold mb-4"> Backend</h3>
                 <div className="flex flex-wrap gap-2">
-                  {tools.map((tech, key) => (
+                  {backendSkills.map((tech, key) => (
                     <span
                       key={key}
                       className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 
@@ -73,7 +73,7 @@ export const About = () => {
               <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
                 <h3 className="text-xl font-bold mb-4"> Tools & Platforms</h3>
                 <div className="flex flex-wrap gap-2">
-                  {backendSkills.map((tech, key) => (
+                  {tools.map((tech, key) => (
                     <span
                       key={key}
                       className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 
