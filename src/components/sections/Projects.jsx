432919d8c27cc3459ea9a7192304064027cec583
anuraag <anuraagkumar7678@gmail.com>
2026-06-19 16:35:44 +0530

    added project

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -187,7 +187,7 @@ export const Projects = () => {
               <div className="flex flex-wrap gap-2 mb-4">
                 {[
                   "Javascript",
-                  "Express",
+                  "Express.js",
                   "React.js",
                   "Recharts",
                   "CoinGecko API",
@@ -205,6 +205,54 @@ export const Projects = () => {
                     {tech}
                   </span>
                 ))}
+              </div>
+              <div className="flex justify-between items-center ">
+                <a
+                  href="#"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-blue-400 hover:text-blue-300 transition-colors my-4"
+                >
+                  View Project →
+                </a>
+              </div>
+            </div>
+            <div
+              className="
+              glass p-6 rounded-xl border border-white/10 
+              hover:-translate-y-1 hover:border-blue-500/30
+              hover:shadow-[0_4px_20px_rgba(59,130,246,0.1)]
+              transition-all
+            "
+            >
+              <img
+                src="project6.png"
+                alt="Project 1"
+                className="w-full h-70  rounded mb-4"
+              />
+              <h3 className="text-xl font-bold mb-2">Game Listing Website</h3>
+              <p className="text-gray-400 mb-4">
+                Built a modern gaming hub concept featuring a sleek dark-themed
+                UI, intuitive genre-based filtering, and prominent hero sections
+                for featured titles.
+              </p>
+              <div className="flex flex-wrap gap-2 mb-4">
+                {["Javascript", "Express.js", "React.js", "TailwindCSS"].map(
+                  (tech, key) => (
+                    <span
+                      key={key}
+                      className="
+                      bg-blue-500/10 text-blue-500 py-1 px-3 
+                      rounded-full text-sm
+                      transition
+                      hover:bg-blue-500/20 hover:-translate-y-0.5
+                      hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)]
+                    "
+                    >
+                      {tech}
+                    </span>
+                  ),
+                )}
               </div>
               <div className="flex justify-between items-center ">
                 <a
